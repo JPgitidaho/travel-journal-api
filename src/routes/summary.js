22d68ede95d18d6d0a9000e2f6b471f88c8484ff
JPgitidaho <juanichile2015@gmail.com>
2025-10-15 15:37:20 -0300

    actualization routes sumary

--- src/routes/summary.js
+++ src/routes/summary.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getSummary } from "../controllers/summaryController.js"
+import { getSummary } from "../controllers/summaryController"
 
 const router = express.Router()
 router.get("/all", getSummary)
